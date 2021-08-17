1c9143c0c3262de76dd2846cdfd1d232ffc28514
MMaddoxx <nontouch16974@gmail.com>
2021-08-17 21:18:41 +0700

    Finish
    Program 2

---
+++ Lab3/Px.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int b=0,i,n,check=0,a=0,ans[999];
+    for(i=1;i<=10000;i++)//ไล่เลขตั้งแต่ 1 ถึง 10000
+    {
+        for(n=1;n<=i/2;n++)//หาตัวที่หารลงตัว โดยใช้เลขตั้งแต่ 1 ถึง i/2 
+        {
+            if(i%n==0)//ถ้าหารลงตัว 
+            {
+                check=check+n;
+            }
+        }
+        if(i==check)//ถ้าผลรวมเท่ากับ i 
+        {
+            ans[a]=check;
+            a++;
+        }   
+        check=0;
+    }
+    for(n=0;n<a;n++)
+    {
+        printf("%d\t",ans[n]);
+    }
+}

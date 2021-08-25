c8cf0fd2e236937fb57e98dd9a8f635045c54ff0
MMaddoxx <nontouch16974@gmail.com>
2021-08-25 11:02:38 +0700

    Finish

--- Lab4/Trianglearea.c
+++ Lab4/Trianglearea.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<math.h>
+#include<conio.h>
 int main()
 {
     int i=0,j=0;
-    float num[3],a,s;
+    float num[3],a,s=0;
     printf("Enter length of the sides of the triangle : ");
     scanf("%f %f %f",&num[0] ,&num[1] ,&num[2]);
     for (i = 0; i < 3; i++)//เอาไว้เรียงตัวเลขที่inputเข้ามาจากน้อยไปหามาก
@@ -18,7 +19,7 @@ int main()
             }
         }
     }
-    if(num[2]<=num[1]+num[0])//เป็นสามเหลี่ยม
+    if(num[2]<num[1]+num[0])//เป็นสามเหลี่ยม
     {
         s=(num[0]+num[1]+num[2])/2;  
         printf("It is Triange and the area is %.2f",sqrt(s*(s-num[0])*(s-num[1])*(s-num[2])));
@@ -27,4 +28,6 @@ int main()
     {
         printf("0");
     }
+    getch();
+    return 0;
 }

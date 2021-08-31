144e99ac3590f7b81f381789974429a9f6c8b0ae
MMaddoxx <nontouch16974@gmail.com>
2021-08-31 17:23:34 +0700

    Finish

---
+++ Lab5/SumAssignment.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<conio.h>
+#include<Windows.h>
+#include<stdlib.h>
+void drawship();
+void erase_ship();
+int main()
+{
+    char t;
+    int x=20,y=20;
+    printf("Press wasd to move and press x to quit");
+    drawship(x,y);
+    do
+    {
+        if(kbhit())
+        {
+            t=getch();
+            if(t=='a')
+            {
+                if(x>=1)
+                {
+                    erase_ship(x,y);
+                    drawship(--x,y);
+                    Sleep(500);
+                }
+            }
+            if(t=='d')
+            {
+                if(x<=80)
+                {
+                    erase_ship(x,y);
+                    drawship(++x,y);
+                    Sleep(500);
+                }
+            }
+            if(t=='w')
+            {
+                if(y>=3)
+                {
+                    erase_ship(x,y);
+                    drawship(x,--y);
+                    Sleep(500);
+                }
+            }
+            if(t=='s')
+            {
+                erase_ship(x,y);
+                drawship(x,++y);
+                Sleep(500);
+            }
+        }
+    } while (t!='x');
+    return 0;
+}
+void drawship(int x , int y)
+{
+    COORD c ={x,y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+    printf("<-o->");
+}
+void erase_ship(int x, int y)
+{
+    COORD c ={x,y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+    printf("     ");
+}
